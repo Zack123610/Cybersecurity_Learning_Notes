df0579dfd9b476561fa3b5d8797a1b7ed7e7fbd8
Zack123610 <zack123610@gmail.com>
2023-01-14 02:53:15 +0000

    updated yahoofinance_v2

--- Web Scraping/yahoofinance_v2.py
+++ Web Scraping/yahoofinance_v2.py
@@ -5,20 +5,29 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://sg.finance.yahoo.com/quote/AAPL/financials?p=AAPL"
-headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"}
-page = requests.get(url, headers=headers)
-page_content = page.content
-soup = BeautifulSoup(page_content, "html.parser")
-tabl = soup.find_all("div", {"class" : "M(0) Whs(n) BdEnd Bdc($seperatorColor) D(itb)"})
 
-income_statement = {}
+tickers = ["AAPL", "AMZN", "META", "GOOG", "3988.HK"]
+income_statement_dict = {}
+balance_sheet_dict = {}
+cashflow_st_dict = {}
 
-for t in tabl:
-    rows = t.find_all("div", {"class" : "D(tbr) fi-row Bgc($hoverBgColor):h"})
-    for row in rows:
-        try:
-            print(row.get_text(separator="|").split("|"))
-            income_statement[row.get_text(separator="|").split("|")[0]] = row.get_text(separator="|").split("|")[1]
-        except:
-            print("This row " + row.get_text() + " has no value")
+for ticker in tickers:
+    url = f"https://sg.finance.yahoo.com/quote/{ticker}/financials?p={ticker}"
+    headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"}
+    page = requests.get(url, headers=headers)
+    page_content = page.content
+    soup = BeautifulSoup(page_content, "html.parser")
+    tabl = soup.find_all("div", {"class" : "M(0) Whs(n) BdEnd Bdc($seperatorColor) D(itb)"})
+
+    income_statement = {}
+
+    for t in tabl:
+        heading = t.find_all()
+
+        rows = t.find_all("div", {"class" : "D(tbr) fi-row Bgc($hoverBgColor):h"})
+        for row in rows:
+            try:
+                print(row.get_text(separator="|").split("|"))
+                income_statement[row.get_text(separator="|").split("|")[0]] = row.get_text(separator="|").split("|")[1]
+            except:
+                print("This row " + row.get_text() + " has no value")
